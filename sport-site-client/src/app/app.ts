f6d2025fa4d6c9a6084e447f00f8c1810d2020f7
Martin7n <122726440+Martin7n@users.noreply.github.com>
2025-07-29 22:13:39 +0300

    imports

--- sport-site-client/src/app/app.ts
+++ sport-site-client/src/app/app.ts
@@ -1,11 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './layout/navigation/navigation.component';
 import {  ComplexLibrarySample } from './features/complexes/complex-library-sample/complex-library-sample';
 import { FooterComponent } from './layout/footer/footer.component';
 import { Register } from './features/users/register/register';
 import { Login } from './features/users/login/login';
 import { ActionGrid } from './home/action-grid/action-grid';
+import { AuthService } from './core/services/auth-service/auth.service';
 
 
 // @Component({
